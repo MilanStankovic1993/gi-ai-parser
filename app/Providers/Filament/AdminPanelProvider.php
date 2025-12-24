86e4c24462aa587f463181271050f922ac45bab1
Milan Radijator <milan.stankovic@radijator.rs>
2025-12-24 13:59:05 +0100

    * dodajem logo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,10 +26,22 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+
+            // LOGIN
             ->login()
+
+            //  BRANDING
+            ->brandName('Grcka-info AI platforma')
+            ->brandLogo(asset('images/grckainfo-logo.svg'))
+            ->brandLogoHeight('2rem')
+            ->favicon(asset('images/grckainfo-logo.svg'))
+
+            // BOJE
             ->colors([
                 'primary' => Color::Amber,
             ])
+
+            // RESOURCES / PAGES / WIDGETS
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
@@ -38,8 +50,10 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // Widgets\FilamentInfoWidget::class,
             ])
+
+            // MIDDLEWARE
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
